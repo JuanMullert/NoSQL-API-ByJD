a7718aa1413ca2a2437307d834d74a5eccd49d93
JuanMullert <114509238+JuanMullert@users.noreply.github.com>
2023-02-28 21:05:55 -0600

    provided module that exports methods

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -46,3 +46,49 @@ const thoughtController = {
                 res.json(dbPizzaData);
             })
             .catch(err => res.json(err));
+        },
+        deleteThought({ params }, res) {
+            Thought.findByIdAndDelete({ _id: params.thoughtId }, { runValidators: true, new: true })
+                .then(thoughtData => {
+                    if (!thoughtData) {
+                        res.status(404).json({ message: 'No user found with this ID!' });
+                        return;
+                    }
+                    res.json(dbPizzaData);
+                })
+                .catch(err => res.json(err));
+        },
+        addReaction({params, body}, res){
+            Thought.findOneAndUpdate(
+                {_id: params.thoughtId},
+                {$push: {reactions: body}},
+                { new: true, runValidators: true }
+            )
+            .then(thoughtData => {
+                if (!thoughtData) {
+                    res.status(404).json({ message: 'Incorrect reaction data!' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.json(err));
+        },
+        deleteReaction({params}, res){
+            Thought.findOneAndUpdate(
+                {_id: params.thoughtId},
+                {$pull: {reactions: {reactionId : params.reactionId}}},
+                { new: true, runValidators: true }
+            )
+            .then(thoughtData => {
+                if (!thoughtData) {
+                    res.status(404).json({ message: 'Incorrect reaction data!' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.json(err));
+        }
+    }
+    
+    
+    module.exports = thoughtController;
--- server.js
+++ server.js
@@ -9,3 +9,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/noSQL-project', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`Connected on localhost: ${PORT}`));
