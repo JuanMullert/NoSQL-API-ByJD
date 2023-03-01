25f64c799514222470b624cb6170ac558dfc9a2d
JuanMullert <114509238+JuanMullert@users.noreply.github.com>
2023-02-28 20:44:13 -0600

    export mongoose and worked on controllers

---
+++ controllers/thoughtController.js
@@ -0,0 +1,48 @@
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+
+    getThoughts(req, res) {
+        Thought.find({})
+            .then(thoughtData => res.json(thoughtData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+    getThoughtByID({ params }, res) {
+        Thought.findOne({ _id: params.thoughtId })
+            .then(thoughtData => res.json(thoughtData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+    addThought({ params, body }, res) {
+        Thought.create(body)
+            .then(({ _id }) => {
+                return User.findOneAndUpdate(
+                    { _id: params.userId },
+                    { $push: { thoughts: _id } },
+                    { new: true }
+                );
+            })
+            .then(thoughtData => {
+                if (!thoughtData) {
+                    res.status(404).json({ message: 'Incorrect thought data!' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.json(err));
+    },
+    updateThought({ params, body }, res) {
+        Thought.findByIdAndUpdate({ _id: params.thoughtId }, body, { runValidators: true, new: true })
+            .then(thoughtData => {
+                if (!thoughtData) {
+                    res.status(404).json({ message: 'No user found with this ID!' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.json(err));
