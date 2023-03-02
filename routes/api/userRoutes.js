8b3ae9d8c17dc448d3c03712bdab8352de7da813
JuanMullert <114509238+JuanMullert@users.noreply.github.com>
2023-03-01 18:01:11 -0600

    fixed bug on user routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,7 +2,12 @@ const router = require('express').Router();
 
 const {
     getUsers,
-    addUser
+    addUser,
+    getUserByID,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
 } = require('../../controllers/user-controller');
 
 // /api/users
@@ -10,6 +15,17 @@ router.route('/')
 .get(getUsers)
 .post(addUser);
 
+// /api/users/:id
+router.route('/:id')
+.get(getUserByID)
+.put(updateUser)
+.delete(deleteUser);
+
+// /api/users/:id/friends/:friendId
+
+router.route('/:id/friends/:friendID')
+.post(addFriend)
+.delete(deleteFriend)
 
 
 module.exports = router;
